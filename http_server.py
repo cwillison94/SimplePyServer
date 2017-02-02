15b149415518243de3f82819d768fdd955569aa5
cwillison94 <cwillison94@gmail.com>
2017-02-01 23:58:45 -0500

    Added 500 internal server error
    added internal server error to handle general error case

--- http_server.py
+++ http_server.py
@@ -11,19 +11,23 @@ EOL = "\r\n"
 
 STATUS_200_OK = 200
 STATUS_404_NOT_FOUND = 404
+STATUS_500_INTERNAL_SERVER_ERROR = 500
 
 CONTENT_TPYE_HTML = 1
 CONTENT_TYPE_IMAGE = 2
 
 ERROR_404_HTML = "<html><b style='color: red'>404 Not Found</b></html>"
+INTERNAL_ERROR_500_HTML = "<html><b style='color: red'>500 Internal Server Error</b></html>"
 
 
 def constructResponse(content, status, contentType):
 
     if status == STATUS_200_OK:
         response = bytearray("HTTP/1.1 200 OK" + EOL)
-    else:
+    elif status == STATUS_500_INTERNAL_SERVER_ERROR:
         response = bytearray("HTTP/1.1 404 Not Found")
+    else: #STATUS_500_INTERNAL_SERVER_ERROR
+        response = bytearray("HTTP/1.1 500 Internal Server Error")
 
     response.extend("Data: " + formatdate(timeval=None, localtime=False, usegmt=True) + EOL)
     response.extend("Accept-Ranges: bytes" + EOL)
@@ -35,7 +39,6 @@ def constructResponse(content, status, contentType):
         response.extend("Content-Type: image" + EOL)
 
     response.extend(EOL)
-
     response.extend(content)
 
     return response
@@ -43,9 +46,9 @@ def constructResponse(content, status, contentType):
 def getContentType(filename):
     if filename.lower().endswith(".html"):
         return CONTENT_TPYE_HTML
+    #elif filename.lower().endswith(".png")
     else:
         return CONTENT_TYPE_IMAGE
-    #elif filename.lower().endswith(".png")
 
 def sendRepsone(connectionSocket, response):
     connectionSocket.send(response)
@@ -56,7 +59,7 @@ def sendRepsone(connectionSocket, response):
 def startServer(host , port):
     serverSocket = socket(AF_INET, SOCK_STREAM) 
 
-    serverSocket.bind((host, post))
+    serverSocket.bind((host, port))
     serverSocket.listen(1)
 
     while True: 
@@ -65,9 +68,9 @@ def startServer(host , port):
         connectionSocket, addr = serverSocket.accept()
         print "connected by ", addr
         try:
-            message = connectionSocket.recv(1024)
-            filename = message.split()[1][1:]
-
+            request = connectionSocket.recv(1024)
+            filename = request.split()[1][1:]
+            
             f = open(filename, "rb") 
             content = f.read()
 
@@ -80,6 +83,13 @@ def startServer(host , port):
             content = bytearray(ERROR_404_HTML)
             
             response = constructResponse(content, STATUS_404_NOT_FOUND, CONTENT_TPYE_HTML)
+            sendRepsone(connectionSocket, response)
+
+        except Exception:
+
+            content = bytearray(INTERNAL_ERROR_500_HTML)
+
+            response = constructResponse(content, STATUS_500_INTERNAL_SERVER_ERROR, CONTENT_TPYE_HTML)
             sendRepsone(connectionSocket, response)
 
         connectionSocket.close()
